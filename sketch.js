166e7edc8345d4236d5de44e11b30a475b15ae1a
Aryaan <aryaanmerchant@gmail.com>
2020-11-03 17:36:32 +0530

    added

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
+var platform;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
@@ -14,10 +15,10 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+   
     
     ground = new Ground(600,height,1200,20)
-
+    platform = new Ground(150,305,300,170)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -48,6 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    platform.display();
 
     box3.display();
     box4.display();
